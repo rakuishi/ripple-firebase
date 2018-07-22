076f2cd3d28c84c1411ac25da60975c5c30afb25
rakuishi <rakuishi@gmail.com>
2018-07-22 17:28:01 +0900

    Using Express request and response objects

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
-const functions = require('firebase-functions');
+'use strict';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+app.get('/balance', (req, res) => {
+  axios.get('https://api.coinmarketcap.com/v2/ticker/52/?convert=JPY')
+    .then(response => {
+      const jpy = response.data.data.quotes.JPY.price;
+      const json = JSON.stringify({ jpy: jpy });
+      res.header('Content-Type', 'application/json');
+      res.end(json);
+      return json;
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
